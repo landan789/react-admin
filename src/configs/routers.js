49fb3b5e8c5c458322c068291f4e06fc67d6e133
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-12 16:08:31 +0800

    [feature/initial]
    second commit

---
+++ src/configs/routers.js
@@ -0,0 +1,8 @@
+const ROUTES = {
+    MEMBER: '/member',
+    FINANCE: '/finance',
+    SIGNUP: '/signup',
+    SIGNOUT: '/signout'
+};
+
+export default ROUTES;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './pages/App';
+import './index.css';
+
+// import registerServiceWorker from './registerServiceWorker';
+
+import ROUTERS_CFG from './configs/routers';
+
+const routes = [{
+    path: ROUTERS_CFG.MEMBER,
+    component: App,
+    exact: true
+}];
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+        {routes.map((route) => (
+            <Route key={route.path}
+                exact={route.exact}
+                path={route.path}
+                component={route.component} />
+        ))}
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
+// registerServiceWorker();

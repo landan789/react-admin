def41f8338a592dbec27d430554fbdd6bb572381
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-12 21:44:08 +0800

    [feature/sidebar]

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,9 +2,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
+import AboutPage from './../pages/About';
+import ContactPage from './../pages/Contact';
+import HomePage from './../pages/Home';
 import MemberPage from './../pages/Member';
 
 const routes = [{
+    path: '/about',
+    component: AboutPage,
+    exact: true
+}, {
+    path: '/contact',
+    component: ContactPage,
+    exact: true
+}, {
+    path: '/home',
+    component: HomePage,
+    exact: true
+}, {
     path: '/member',
     component: MemberPage,
     exact: true
